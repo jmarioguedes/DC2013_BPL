bad95286b206cb70a49c159ff2c31bd32d3b1a45
Mário Guedes <jmarioguedes@gmail.com>
2013-10-31 22:18:38 -0200

    Final

--- 01 - Ola Mundo/02 - Modularizado/02 - Módulo/DC2013.OlaMundo.OlaMundo.pas
+++ 01 - Ola Mundo/02 - Modularizado/02 - Módulo/DC2013.OlaMundo.OlaMundo.pas
@@ -8,7 +8,7 @@ implementation
 
 function GerarOlaMundo: string;
 begin
-  Result := ' - Delphi Conference 2013 - '; // --> Regra de negócio
+  Result := ' - Embarcadero Conference 2013 - '; // --> Regra de negócio
 end;
 
 end.
